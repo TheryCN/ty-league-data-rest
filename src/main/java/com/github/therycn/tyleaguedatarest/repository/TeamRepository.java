5b4a4ea489421d17b90ee58353a0fa667158f622
THERY <thery.charasson@pistenbully.fr>
2021-06-11 11:05:19 +0200

    Add custom query example

--- src/main/java/com/github/therycn/tyleaguedatarest/repository/TeamRepository.java
+++ src/main/java/com/github/therycn/tyleaguedatarest/repository/TeamRepository.java
@@ -1,7 +1,16 @@
 package com.github.therycn.tyleaguedatarest.repository;
 
 import com.github.therycn.tyleaguedatarest.entity.Team;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
+
+	// Resource will be accessible at `/teams/search/nameStartsWith?name=`
+	@RestResource(path = "nameStartsWith", rel = "nameStartsWith")
+	Page<?> findByNameStartsWith(@Param("name") String name, Pageable pageable);
+
 }
